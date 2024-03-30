9b46893bd32391f683d70d5364886094f1dae8d4
Jona <llamasjonat@gmail.com>
2024-03-30 18:18:39 -0300

    feat(page): added Profile

---
+++ apps/web/app/profile/[lud16]/page.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Container, Divider, Flex, Text, Button, Heading } from '@lawallet/ui';
+
+import { Avatar } from '@/components/Avatar';
+import Ticket from '@/components/Icons/Ticket';
+import Bolt from '@/components/Icons/Bolt';
+import Badge from '@/components/Badge';
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@/components/Tabs';
+
+import { appTheme } from '../../../config/exports';
+import Progress from '@/components/Progress';
+
+export default function Page(): JSX.Element {
+  const hasTicket = true;
+  const valueProgress = 10;
+  const isDeath = false;
+
+  const [showTab, setTab] = useState('rondas');
+
+  return (
+    <>
+      <Divider y={64} />
+      <Container size="small">
+        <Flex justify="space-between" align="center">
+          <Avatar src="https://static.lawallet.io/img/domains/8333.mobi.png" alt="Machankura" size={20} />
+          <div>
+            {hasTicket ? (
+              <Button onClick={() => null} variant="bezeled" disabled={isDeath}>
+                <Bolt />
+                Agregar poder
+              </Button>
+            ) : (
+              <Button onClick={() => null} variant="bezeled">
+                <Ticket />
+                Comprar ticket
+              </Button>
+            )}
+          </div>
+        </Flex>
+        <Divider y={8} />
+        <Text isBold>dios@lawallet.ar</Text>
+        <Divider y={8} />
+        <Text size="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</Text>
+        <Divider y={12} />
+        <div>
+          {hasTicket ? <Badge color="primary">Ticket adquirido</Badge> : <Badge color="secondary">Sin Ticket</Badge>}
+        </div>
+        <Divider y={12} />
+        <Text size="small" color={appTheme.colors.gray50}>
+          Chances de sobrevivir la ronda
+        </Text>
+        <Divider y={8} />
+        <Flex align="center" gap={8}>
+          <Progress value={valueProgress} />
+          <Text>{valueProgress}%</Text>
+        </Flex>
+        <Divider y={12} />
+
+        {/* Configurar para que al hacer click en la Tab se muestre el TabPanel correspondiente */}
+        <Tabs>
+          <TabList>
+            <Tab active={showTab === 'rondas'} onClick={() => setTab('rondas')}>
+              Rondas
+            </Tab>
+            <Tab active={showTab === 'zapeos'} onClick={() => setTab('zapeos')}>
+              Zapeos
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel show={showTab === 'rondas'}>
+              <Flex direction="column" flex={1} align="center" justify="center">
+                <Heading as="h4">Lorem, ipsum dolor.</Heading>
+                <Text align="center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, impedit.</Text>
+              </Flex>
+            </TabPanel>
+            <TabPanel show={showTab === 'zapeos'}>zapeos</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Container>
+    </>
+  );
+}
