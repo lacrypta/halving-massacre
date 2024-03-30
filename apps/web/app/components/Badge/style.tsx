74154aedc6ef5b8a9655d780c5c28b93d2295337
Jona <llamasjonat@gmail.com>
2024-03-30 18:17:35 -0300

    feat(component): added Badge

---
+++ apps/web/app/components/Badge/style.tsx
@@ -0,0 +1,19 @@
+import { styled } from 'styled-components';
+
+import { appTheme } from '../../../config/exports';
+
+interface BadgePrimitiveProps {
+  $color: 'primary' | 'secondary';
+}
+
+export const BadgePrimitive = styled.div<BadgePrimitiveProps>`
+  display: inline-flex;
+  width: auto;
+
+  padding: 2px 10px;
+
+  background-color: ${appTheme.colors.gray20};
+  border-radius: 12px;
+
+  color: ${(props) => appTheme.colors[props.$color]};
+`;
