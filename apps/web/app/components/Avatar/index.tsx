e3ac63f0ea3fe8302afc3c31f1dceb24ad09ecc7
Agustin Kassis <webmaster@masize.com>
2024-03-30 18:58:47 -0300

    chore: tidy up avatar

--- apps/web/app/components/Avatar/index.tsx
+++ apps/web/app/components/Avatar/index.tsx
@@ -2,15 +2,13 @@ import Image from 'next/image';
 
 import { AvatarPrimitive } from './style';
 
-interface AvatarPrimitive {
+interface AvatarProps {
   src: string;
   alt: string;
   size: number;
 }
 
-export function Avatar(props: AvatarPrimitive) {
-  const { src, alt, size } = props;
-
+export function Avatar({ src, alt, size }: AvatarProps) {
   return (
     <AvatarPrimitive $size={size}>
       <Image src={src} alt={alt} fill />
