fb719a9e6edd804c32f6c25959489fec616bb0a7
Agustin Kassis <webmaster@masize.com>
2024-03-30 20:36:59 -0300

    added multiple relays

--- apps/web/app/template.tsx
+++ apps/web/app/template.tsx
@@ -1,14 +1,30 @@
-"use client";
-import { NextProvider } from "@lawallet/ui/next";
-import "@lawallet/ui/styles";
-import { appTheme } from "../config/exports";
-import Footer from "./components/Footer";
+'use client';
+import { NextProvider } from '@lawallet/ui/next';
+import { NostrProvider, ProfileCacheProvider, createConfig } from '@lawallet/react';
+import '@lawallet/ui/styles';
+import { appTheme } from '../config/exports';
+import Footer from './components/Footer';
+
+const relaysList: string[] = [
+  'wss://relay.hodl.ar',
+  'wss://relay.lawallet.ar',
+  'wss://relay.primal.net',
+  'wss://relay.snort.social',
+  'wss://nos.lol',
+  'wss://relay.damus.io',
+  'wss://nostr-pub.wellorder.net',
+];
+const nostrConfig = createConfig({ relaysList });
 
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <NextProvider theme={appTheme}>
-      {children}
-      <Footer />
+      <NostrProvider config={nostrConfig}>
+        <ProfileCacheProvider>
+          {children}
+          <Footer />
+        </ProfileCacheProvider>
+      </NostrProvider>
     </NextProvider>
   );
 }
