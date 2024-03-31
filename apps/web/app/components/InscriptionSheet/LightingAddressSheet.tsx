6a8e4f1b12c0f4af28f6a1b44fb2f9e1457204d1
Agustin Kassis <webmaster@masize.com>
2024-03-31 02:46:09 -0300

    tify up LightningAddress sheet

--- apps/web/app/components/InscriptionSheet/LightingAddressSheet.tsx
+++ apps/web/app/components/InscriptionSheet/LightingAddressSheet.tsx
@@ -1,26 +1,9 @@
-import {
-  Button,
-  Container,
-  Divider,
-  Flex,
-  Heading,
-  Sheet,
-  Text,
-} from "@lawallet/ui";
-import { useState } from "react";
+import { Button, Container, Divider, Flex, Heading, Sheet, Text } from '@lawallet/ui';
+import Link from 'next/link';
 
-const LightingAddressSheet = ({
-  isOpen,
-  onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
-  const [invoice, setInvoice] = useState<string>("");
-  console.log(invoice);
-
+const LightingAddressSheet = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
   return (
-    <Sheet title={""} isOpen={isOpen} closeText={"Cerrar"} onClose={onClose}>
+    <Sheet title={''} isOpen={isOpen} closeText={'Cerrar'} onClose={onClose}>
       {/* <Flex align="center" justify="center"> */}
       <Heading as="h3" align="center">
         ¿Qué es Lightning Address?
@@ -30,14 +13,14 @@ const LightingAddressSheet = ({
 
       <Container>
         <Text align="center" isBold>
-          Como una dirección de correo electrónico, ¡pero para tu Bitcoin!
+          Como una dirección de correo electrónico, ¡pero para Bitcoin!
         </Text>
 
         <Divider y={20} />
 
         <Text align="center">
-          Una forma enormemente más sencilla para que cualquiera pueda enviarle
-          Bitcoin al instante en Lightning Network.
+          Una forma enormemente más sencilla para que cualquiera pueda enviarle Bitcoin al instante en Lightning
+          Network.
         </Text>
 
         <Divider y={20} />
@@ -47,7 +30,9 @@ const LightingAddressSheet = ({
         <Divider y={20} />
 
         <Flex direction="column" align="center">
-          <Button onClick={() => setInvoice("prueba")}>Obtener el mio</Button>
+          <Link href="https://app.lawallet.ar/signup" rel="noopener noreferrer" target="_blank">
+            <Button>Obtener el mio</Button>
+          </Link>
         </Flex>
       </Container>
     </Sheet>
