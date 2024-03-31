1da414e7067c8100491dbd3b6f5ec4ae891d4216
Agustin Kassis <webmaster@masize.com>
2024-03-31 02:45:56 -0300

    tidy up inscription sheet

--- apps/web/app/components/InscriptionSheet/InscriptionSheet.tsx
+++ apps/web/app/components/InscriptionSheet/InscriptionSheet.tsx
@@ -1,94 +1,60 @@
-import {
-  ActionSheet,
-  Button,
-  Container,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Text,
-} from "@lawallet/ui";
-import { type ChangeEvent, useState } from "react";
-import Link from "../Icons/Link";
-import LightingAddressSheet from "./LightingAddressSheet";
-import Payment from "./Payment";
+import { ActionSheet, Button, Container, Divider, Flex, Heading, Text } from '@lawallet/ui';
+import { useState } from 'react';
+import Payment from './Payment';
+import RulesSheet from '../Rules/RulesSheet';
 
-const InscriptionSheet = ({
-  isOpen,
-  onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
-  const [openLNInfo, setOpenLNInfo] = useState(false);
-  const [lnAddress, setLNAddress] = useState<string>("");
-  const [invoicePayment, setInvoicePayment] = useState<string>("");
+const InscriptionSheet = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
+  const [isRulesOpen, setIsRulesOpen] = useState(false);
+  const [invoicePayment, setInvoicePayment] = useState<string>('');
 
   const handleClick = () => {
-    setInvoicePayment("prueba");
-  };
-
-  const handleChangeAddress = (e: ChangeEvent<HTMLInputElement>) => {
-    const address = e.target.value;
-    setLNAddress(address);
+    setInvoicePayment('prueba');
   };
 
   const restartModal = () => {
-    setInvoicePayment("");
-    setLNAddress("");
+    setInvoicePayment('');
     onClose();
   };
 
   return (
-    <ActionSheet
-      isOpen={isOpen}
-      description=""
-      onClose={restartModal}
-      cancelText="Cancelar"
-    >
+    <ActionSheet isOpen={isOpen} description="" onClose={restartModal} cancelText="Cancelar">
       {!invoicePayment ? (
         <>
           <Container>
             <Flex flex={1} justify="center" align="center" direction="column">
-              <Heading as="h3">Anotate ahora</Heading>
+              <Heading as="h3">Comprá tu ticket</Heading>
 
-              <Divider y={12} />
+              <Divider y={24} />
 
-              <Text align="center">
-                A message should be a short, complete sentence.
+              <Text>
+                El ticket cuesta <b>210 sats</b> (por única vez).
               </Text>
+              <Divider y={20} />
+              <Text>
+                Se te agrega <b>1 sat de poder</b>
+              </Text>
+              <Divider y={20} />
+              <Text>Podes seguir agregando poder cuando quieras mientras sigas vivo</Text>
 
               <Divider y={12} />
-
-              <Flex
-                flex={1}
-                direction="row"
-                align="center"
-                justify="center"
-                onClick={() => setOpenLNInfo(true)}
-              >
-                <Text color="#56B68C">¿Qué es esto?</Text>
-                <Link />
-              </Flex>
-
-              <Divider y={12} />
-
-              <Input
-                onChange={handleChangeAddress}
-                placeholder="Lightning Address"
-                value={lnAddress}
-              />
             </Flex>
 
             <Divider y={12} />
 
-            <Button onClick={handleClick}>Anotarme</Button>
+            <Flex flex={1} direction="row" align="center" justify="center">
+              <Button onClick={handleClick}>Anotarme</Button>
+            </Flex>
+
+            <Divider y={6} />
+
+            <Flex flex={1} direction="row" align="center" justify="center" onClick={() => setIsRulesOpen(true)}>
+              <Text size="small" color="#56B68C">
+                Ver reglamento
+              </Text>
+            </Flex>
           </Container>
 
-          <LightingAddressSheet
-            isOpen={openLNInfo}
-            onClose={() => setOpenLNInfo(false)}
-          />
+          <RulesSheet isOpen={isRulesOpen} onClose={() => setIsRulesOpen(false)} />
         </>
       ) : (
         <Payment invoice={invoicePayment} />
