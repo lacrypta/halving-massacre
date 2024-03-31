38cfb44f8d182b30beb233f77cd2aa96adcfd7b0
Agustin Kassis <webmaster@masize.com>
2024-03-31 02:45:08 -0300

    added keypress hook

---
+++ apps/web/hooks/useActionOnKeypress.ts
@@ -0,0 +1,28 @@
+import { type DependencyList, useEffect } from 'react';
+
+export interface IUseActionOnEnter {
+  // eslint-disable-next-line no-unused-vars
+  handlePressKey: (e: KeyboardEvent) => void;
+}
+
+export const useActionOnKeypress = (
+  key: string,
+  action: () => void,
+  dependences: DependencyList,
+  condition?: boolean,
+): IUseActionOnEnter => {
+  const handlePressKey = async (e: KeyboardEvent) => {
+    e.stopPropagation();
+    if (e.key === key && !condition) action?.();
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handlePressKey);
+
+    return () => {
+      window.removeEventListener('keydown', handlePressKey);
+    };
+  }, [...dependences]);
+
+  return { handlePressKey };
+};
