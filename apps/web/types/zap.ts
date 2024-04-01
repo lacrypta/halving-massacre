152ef1f6684a00b8446660690d1fb7ddfd64ff1a
Agustin Kassis <webmaster@masize.com>
2024-04-01 04:36:02 -0300

    added zap type

--- apps/web/types/zap.ts
+++ apps/web/types/zap.ts
@@ -1,6 +1,15 @@
+import type { Event, EventTemplate } from 'nostr-tools';
+
 export interface Zap {
   author: string; // Walias
   amount: number; // Millisats
   comment?: string; // From zap
   createdAt: number; // Timestamp
+}
+
+export interface ZapReceiptWithCommitment {
+  zapReceipt: Event;
+  zapRequest: Event;
+  commitment: EventTemplate;
+  millisatoshis: number;
 }
