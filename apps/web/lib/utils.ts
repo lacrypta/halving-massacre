ff2e61250516a8151b42f6c5a1c033e28856c6d8
Agustin Kassis <webmaster@masize.com>
2024-04-01 04:36:13 -0300

    added zap parser

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,3 +1,8 @@
+import { validateEvent, type Event } from 'nostr-tools';
+import type { ZapReceiptWithCommitment } from '../types/zap';
+
+import { decode as decodeBolt11 } from 'bolt11';
+
 export function removeObjectKeys(
   obj1: { [key: string]: number },
   obj2: { [key: string]: number },
@@ -26,4 +31,40 @@ export const validateEmail = (email: string): RegExpMatchArray | null => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   );
+};
+
+export const parseZapReceiptWithCommitment = (zapReceipt: Event): ZapReceiptWithCommitment => {
+  if (!validateEvent(zapReceipt)) {
+    throw new Error('Malformed zapReceipt');
+  }
+
+  const zapRequest: Event | false = JSON.parse(zapReceipt.tags.find((tag) => tag[0] === 'description')?.[1] || 'false');
+  if (!zapRequest) {
+    throw new Error('Zap Request not found on Zap Receipt. This is probably a relay error!');
+  }
+
+  if (!validateEvent(zapRequest)) {
+    throw new Error('Malformed zapRequest');
+  }
+
+  const bolt11 = zapReceipt.tags.find((tag) => tag[0] === 'bolt11')?.[1] as string;
+
+  if (!bolt11) {
+    throw new Error('Malformed Zap Receipt. No bolt11 detected');
+  }
+
+  const millisatoshis = parseInt(decodeBolt11(bolt11).millisatoshis!);
+
+  const commitment: Event | false = JSON.parse(zapRequest.tags.find((tag) => tag[0] === 'commitment')?.[1] as string);
+
+  if (!commitment) {
+    throw new Error('Commitment not found on Zap Request');
+  }
+
+  return {
+    zapReceipt,
+    zapRequest,
+    commitment,
+    millisatoshis,
+  };
 };
