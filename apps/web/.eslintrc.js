4e209f9e5f758ae3c2ff9c463c5429df93f068a8
Agustin Kassis <webmaster@masize.com>
2024-04-02 13:56:26 -0300

    fixed linter

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,12 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@lawallet/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@lawallet/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
+  },
+  globals: {
+    NodeJS: true,
   },
 };
