b7d8f6c0020962dbf8e5cd2991f10fd5056c90c8
Agustin Kassis <webmaster@masize.com>
2024-04-03 01:26:56 -0300

    added ticket and power hooks on PlayerContext

--- apps/web/context/PlayerContext.tsx
+++ apps/web/context/PlayerContext.tsx
@@ -1,4 +1,7 @@
 import { createContext } from 'react';
+import { useTicket } from '../hooks/useTicket';
+import type { Event } from 'nostr-tools';
+import { usePowerEvents } from '../hooks/usePowerEvents';
 
 export interface PlayerContextType {
   power: number;
@@ -7,18 +10,25 @@ export interface PlayerContextType {
   isAlive: boolean;
   walias: string;
   hasTicket: boolean;
+  ticketEvent?: Event;
+  zapEvents?: Event[];
 }
 
 export const PlayerContext = createContext({} as PlayerContextType);
 
 export function PlayerProvider({ children }: React.PropsWithChildren & { walias: string }) {
+  const walias = 'agustin@lacrypta.ar';
+  const { hasTicket, ticketEvent } = useTicket(walias);
+  const { events: powerEvents } = usePowerEvents(walias);
   const value = {
     power: 1000,
     isAlive: true,
-    hasTicket: true,
+    hasTicket,
     totalZaps: 4,
     amountZapped: 700,
-    walias: 'agustin@lacrypta.ar',
+    ticketEvent,
+    powerEvents,
+    walias,
   };
 
   return <PlayerContext.Provider value={value}>{children}</PlayerContext.Provider>;
