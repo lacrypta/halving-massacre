38b59f38e685bb97a284ec12abe85b5d173b2f2f
Jona <llamasjonat@gmail.com>
2024-04-03 12:42:15 -0300

    fix(component): changed export for Card on Icon

--- apps/web/app/components/Icon/index.tsx
+++ apps/web/app/components/Icon/index.tsx
@@ -1,14 +1,1 @@
-import React, { type ReactNode } from 'react';
-
-import { IconStyle } from './style';
-
-export interface IconProps {
-  children: ReactNode;
-  size?: 3 | 4 | 5 | 6 | 7 | 8;
-}
-
-export function Icon(props: IconProps) {
-  const { children, size = 3 } = props;
-
-  return <IconStyle $size={size}>{children}</IconStyle>;
-}
+export { Icon } from './Icon';
