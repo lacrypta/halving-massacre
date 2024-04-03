a2baede211b8aaf0f7332ebf75b1cecfb84a715d
Agustin Kassis <webmaster@masize.com>
2024-04-03 00:50:04 -0300

    fix typo

--- apps/web/app/components/Header/index.tsx
+++ apps/web/app/components/Header/index.tsx
@@ -5,10 +5,10 @@ import { HeaderPrimitive } from './style';
 export default function Header() {
   return (
     <HeaderPrimitive>
-      <Heading align='center'>Halving Massacre</Heading>
+      <Heading align="center">Halving Massacre</Heading>
       <Divider y={12} />
-      <Text align='center'>
-        Sobrevivi todas las rondas seguidas aumentando tu poder de minado con Satoshis y gana el increible premio del
+      <Text align="center">
+        Sobrevivi todas las rondas seguidas aumentando tu poder de minado con Satoshis y ganá el increible premio del
         pozo acumulado.
       </Text>
     </HeaderPrimitive>
