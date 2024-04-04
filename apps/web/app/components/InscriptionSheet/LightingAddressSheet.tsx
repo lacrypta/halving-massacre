f18d0d03c977536bde03b308e470a527ee81bb74
Jona <llamasjonat@gmail.com>
2024-04-04 12:42:09 -0300

    fix(component): structure and design for LigthingAddress

--- apps/web/app/components/InscriptionSheet/LightingAddressSheet.tsx
+++ apps/web/app/components/InscriptionSheet/LightingAddressSheet.tsx
@@ -1,33 +1,35 @@
 import { Button, Container, Divider, Flex, Heading, Sheet, Text } from '@lawallet/ui';
 import Link from 'next/link';
+import { appTheme } from '../../../config/exports';
 
 const LightingAddressSheet = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
   return (
-    <Sheet title={''} isOpen={isOpen} closeText={'Cerrar'} onClose={onClose}>
-      {/* <Flex align="center" justify="center"> */}
-      <Heading as="h3" align="center">
-        ¿Qué es Lightning Address?
-      </Heading>
-
-      <Divider y={20} />
-
+    <Sheet title={'¿Qué es Lightning Address?'} isOpen={isOpen} closeText={'Cerrar'} onClose={onClose}>
       <Container>
-        <Text align="center" isBold>
-          Como una dirección de correo electrónico, ¡pero para Bitcoin!
-        </Text>
+        <Text isBold>Como una dirección de correo electrónico, ¡pero para Bitcoin!</Text>
 
-        <Divider y={20} />
+        <Divider y={16} />
 
-        <Text align="center">
-          Una forma enormemente más sencilla para que cualquiera pueda enviarle Bitcoin al instante en Lightning
+        <Text>
+          Una forma enormemente más sencilla para que cualquiera pueda enviarte Bitcoin al instante en Lightning
           Network.
         </Text>
 
-        <Divider y={20} />
+        <Divider y={16} />
 
-        <Text align="center">usuario@lawallet.ar</Text>
+        <Flex direction="column" justify="center" align="center" gap={4}>
+          <Text color={appTheme.colors.gray50} size="small">
+            Ejemplo:
+          </Text>
+          <Flex justify="center">
+            <Text isBold>usuario</Text>
+            <Text isBold color={appTheme.colors.primary}>
+              @lawallet.ar
+            </Text>
+          </Flex>
+        </Flex>
 
-        <Divider y={20} />
+        <Divider y={16} />
 
         <Flex direction="column" align="center">
           <Link href="https://app.lawallet.ar/signup" rel="noopener noreferrer" target="_blank">
