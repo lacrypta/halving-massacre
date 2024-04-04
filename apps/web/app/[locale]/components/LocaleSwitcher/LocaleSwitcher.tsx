3488ba41031196c5ee7787c2f6c28b55037561e5
Jona <llamasjonat@gmail.com>
2024-04-04 15:41:23 -0300

    refactor(component): structure for LocaleSwitcher

--- apps/web/app/[locale]/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ apps/web/app/[locale]/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,17 +1,66 @@
-import { useLocale } from 'next-intl';
-import { locales } from '../../../../config';
-import LocaleSwitcherSelect from './LocaleSwitcherSelect';
+// Libraries
+import { useState, useTransition } from 'react';
+import { useLocale, useTranslations } from 'next-intl';
+import { Button, Flex, Sheet, Container, Text } from '@lawallet/ui';
+
+// New ui-components
+import { Radio } from '../Radio';
+
+// Theme
+import { appTheme } from '../../../../config/exports';
 
 export default function LocaleSwitcher() {
   const locale = useLocale();
+  const t = useTranslations();
+  const [isPending, startTransition] = useTransition();
+
+  const [sheetLanguage, setSheetLanguage] = useState<boolean>(false);
+
+  function changeLanguage(nextLocale: string) {
+    startTransition(() => {
+      const expire = new Date(Date.now() + 86400 * 365 * 1000).toUTCString();
+      document.cookie = `NEXT_LOCALE=${nextLocale}; expires=${expire}; path=/`;
+      window.location.reload();
+    });
+  }
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale}>
-      {locales.map((lang) => (
-        <option key={lang} value={lang}>
-          {`language: ${lang}`}
-        </option>
-      ))}
-    </LocaleSwitcherSelect>
+    <>
+      <Flex align="center" justify="end" gap={8}>
+        <Text size="small" color={appTheme.colors.gray50}>
+          {t('LANGUAGE')}:
+        </Text>
+        <Button size="small" variant="bezeled" onClick={() => setSheetLanguage(!sheetLanguage)}>
+          {locale.toLocaleUpperCase()}
+        </Button>
+      </Flex>
+
+      <Sheet
+        title={t('CHANGE_LANGUAGE')}
+        isOpen={sheetLanguage}
+        closeText={t('CLOSE')}
+        onClose={() => setSheetLanguage(false)}
+      >
+        <Container>
+          <Flex direction="column" flex={1}>
+            <Radio
+              text={t('ENGLISH')}
+              checked={locale === 'en'}
+              onClick={() => {
+                if (locale !== 'en') changeLanguage('en');
+              }}
+            />
+
+            <Radio
+              text={t('SPANISH')}
+              checked={locale === 'es'}
+              onClick={() => {
+                if (locale !== 'es') changeLanguage('es');
+              }}
+            />
+          </Flex>
+        </Container>
+      </Sheet>
+    </>
   );
 }
