960c9a1b60706330ee27cfc3a68b747160a4e46d
Jona <llamasjonat@gmail.com>
2024-04-05 14:50:25 -0300

    fix(component): integrated link for Rules on Navbar

--- apps/web/app/[locale]/components/Navbar/index.tsx
+++ apps/web/app/[locale]/components/Navbar/index.tsx
@@ -1,5 +1,7 @@
-import type { ReactNode } from 'react';
-import { Container, Flex } from '@lawallet/ui';
+import { type ReactNode } from 'react';
+import Link from 'next/link';
+import { useTranslations } from 'next-intl';
+import { Container, Flex, Button } from '@lawallet/ui';
 
 import { NavbarStyle } from './style';
 import LocaleSwitcher from '../LocaleSwitcher/LocaleSwitcher';
@@ -11,14 +13,27 @@ interface NavbarProps {
 export function Navbar(props: NavbarProps) {
   const { children } = props;
 
+  const t = useTranslations();
+
   return (
-    <NavbarStyle>
-      <Container>
-        <Flex flex={1} align="center" justify="space-between">
-          {children}
-          <LocaleSwitcher />
-        </Flex>
-      </Container>
-    </NavbarStyle>
+    <>
+      <NavbarStyle>
+        <Container>
+          <Flex flex={1} align="center" justify="space-between">
+            <Link href="/">{children}</Link>
+            <Flex gap={16} align="center" justify="end">
+              <div>
+                <Link href="/rules">
+                  <Button size="small" variant="borderless">
+                    {t('SEE_RULES')}
+                  </Button>
+                </Link>
+              </div>
+              <LocaleSwitcher />
+            </Flex>
+          </Flex>
+        </Container>
+      </NavbarStyle>
+    </>
   );
 }
