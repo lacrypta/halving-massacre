46327896c72a6106d746a15123cd98f131b96bbd
Agustin Kassis <webmaster@masize.com>
2024-04-05 11:10:05 -0300

    filter power by walias

--- apps/web/hooks/usePowerEvents.tsx
+++ apps/web/hooks/usePowerEvents.tsx
@@ -11,16 +11,19 @@ const MASSACRE_SETUP_ID = process.env.NEXT_PUBLIC_MASSACRE_SETUP_ID!;
 
 export const usePowerEvents = (walias: string): UsePowerEventsReturns => {
   const { setupId, publisherPubkey } = useMassacre();
+  const filters = {
+    kinds: [1112 as NDKKind],
+    '#l': ['power-receipt'],
+    '#e': [MASSACRE_SETUP_ID],
+    authors: [publisherPubkey],
+  } as any;
+
+  if (walias) {
+    filters['#i'] = [walias];
+  }
+
   const { events: powerEvents } = useSubscription({
-    filters: [
-      {
-        kinds: [1112 as NDKKind],
-        '#l': ['power-receipt'],
-        '#e': [MASSACRE_SETUP_ID],
-        '#i': [walias],
-        authors: [publisherPubkey],
-      },
-    ],
+    filters: [filters],
     enabled: !!setupId,
     options: {
       closeOnEose: false,
