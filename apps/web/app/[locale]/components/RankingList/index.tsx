913af43c0c133eb8363a9e3aa392ca8fdd14bd3c
Jona <llamasjonat@gmail.com>
2024-04-06 20:02:08 -0300

    feat: added RankingList

---
+++ apps/web/app/[locale]/components/RankingList/index.tsx
@@ -0,0 +1,52 @@
+import { Flex, Text } from '@lawallet/ui';
+
+import { appTheme } from '../../../../config/exports';
+
+import { AutoAvatar } from '../AutoAvatar';
+import { Icon } from '../Icon';
+import { Bolt } from '../Icons';
+
+import { RankingListStyle, ItemStyle, NumberStyle, WaliasStyle } from './style';
+
+interface RankingListProps {
+  players: any;
+  type?: 'global' | 'massacre';
+}
+
+export function RankingList(props: RankingListProps) {
+  const { players, type = 'global' } = props;
+
+  return (
+    <RankingListStyle>
+      <Flex align="center" gap={4} direction="column" justify="center">
+        {Object.keys(players).map((walias, index) => {
+          return (
+            <ItemStyle key={walias} href={`/profile/${walias}`} title={walias}>
+              <Flex flex={1} align="center" gap={8}>
+                <NumberStyle>
+                  <Text size="small" color={appTheme.colors.gray50}>
+                    {index + 1}.
+                  </Text>
+                </NumberStyle>
+                <Flex align="center" gap={8}>
+                  <AutoAvatar walias={walias} size={12} />
+                  <WaliasStyle>
+                    <Text>{walias}</Text>
+                  </WaliasStyle>
+                </Flex>
+                <Flex align="center" flex={0} gap={4}>
+                  <Icon>
+                    <Bolt color={type === 'global' ? appTheme.colors.primary : appTheme.colors.error} />
+                  </Icon>
+                  <Text color={type === 'global' ? appTheme.colors.primary : appTheme.colors.error}>
+                    {String(players[walias]! / 1000) || '21'}
+                  </Text>
+                </Flex>
+              </Flex>
+            </ItemStyle>
+          );
+        })}
+      </Flex>
+    </RankingListStyle>
+  );
+}
---
+++ apps/web/app/[locale]/components/RankingList/style.tsx
@@ -0,0 +1,47 @@
+import { styled } from 'styled-components';
+import { appTheme } from '../../../../config/exports';
+
+export const RankingListStyle = styled.div`
+  width: 100%;
+`;
+
+interface ItemStyleProps {
+  $isActive?: boolean;
+}
+
+export const ItemStyle = styled.a<ItemStyleProps>`
+  width: 100%;
+
+  padding: 8px;
+
+  background-color: ${(props) => (props.$isActive ? appTheme.colors.gray20 : 'transparent')};
+  border-radius: 50px;
+
+  color: ${appTheme.colors.text};
+
+  &:hover {
+    background-color: ${appTheme.colors.gray20};
+  }
+`;
+
+export const NumberStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  min-width: 40px;
+`;
+
+export const WaliasStyle = styled.div`
+  width: 100%;
+
+  p {
+    width: 100%;
+    max-width: 150px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    @media screen and (min-width: 768px) {
+      max-width: 200px;
+    }
+  }
+`;
