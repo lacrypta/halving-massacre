2ebe42c4f0187f610eb6154e952b34c3a5e4e755
Jona <llamasjonat@gmail.com>
2024-04-06 20:04:18 -0300

    fix(page): integrated RankingList on Ranking

--- apps/web/app/[locale]/ranking/page.tsx
+++ apps/web/app/[locale]/ranking/page.tsx
@@ -3,18 +3,17 @@
 // Libraries
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { ButtonGroup, Container, Divider, Flex, Heading, Text } from '@lawallet/ui';
+import { Container, Divider, Flex, Heading } from '@lawallet/ui';
+
+import { useMassacre } from '../../../hooks/useMassacre';
 
 // Generic components
 import { Navbar } from '../components/Navbar';
 
 import pngIso from '../../../public/images/iso.png';
 import { Link } from '../../../navigation';
-import { AutoAvatar } from '../components/AutoAvatar';
-import { Bolt } from '../components/Icons';
-import { appTheme } from '../../../config/exports';
-import { useRouter } from 'next/navigation';
-import { useMassacre } from '../../../hooks/useMassacre';
+import { RankingList } from '../components/RankingList';
+import { Tab, TabList, Tabs, TabPanel, TabPanels } from '../components/Tabs';
 
 interface PageProps {
   params: {
@@ -23,8 +22,10 @@ interface PageProps {
 }
 
 export default function Page({ params }: PageProps): JSX.Element {
-  const router = useRouter();
   const { top100Players: players } = useMassacre();
+
+  const [nameTab, setNameTab] = useState('global');
+
   return (
     <>
       <Navbar>
@@ -40,27 +41,29 @@ export default function Page({ params }: PageProps): JSX.Element {
           Top 100
         </Heading>
 
-        <Divider y={20} />
+        <Divider y={16} />
         <Container size="small">
-          <Flex align="center" gap={16} direction="column" justify="center">
-            {Object.keys(players).map((walias) => {
-              return (
-                <ButtonGroup key={walias}>
-                  <Flex onClick={() => router.push(`/profile/${walias}`)} align="center" justify="start" gap={16}>
-                    <Flex align="center" gap={8}>
-                      <AutoAvatar walias={walias} size={12} />
-                      <Text>{walias}</Text>
-                    </Flex>
-                    <Flex justify="end">
-                      <Text color={appTheme.colors.primary}>
-                        <Bolt /> {String(players[walias]! / 1000) || '21'} sats
-                      </Text>
-                    </Flex>
-                  </Flex>
-                </ButtonGroup>
-              );
-            })}
-          </Flex>
+          <Tabs>
+            <TabList>
+              <Tab active={nameTab === 'global' && true} onClick={() => setNameTab('global')}>
+                Global
+              </Tab>
+              <Tab active={nameTab === 'massacre' && true} onClick={() => setNameTab('massacre')} disabled={true}>
+                Masacrados
+              </Tab>
+            </TabList>
+          </Tabs>
+          <TabPanels>
+            <TabPanel show={nameTab === 'global' && true}>
+              <Divider y={16} />
+              <RankingList players={players} />
+            </TabPanel>
+            <TabPanel show={nameTab === 'massacre' && true}>
+              <Divider y={16} />
+              {/* Agregar listado de jugadores massacrados */}
+              {/* <RankingList players={players} type="massacre" /> */}
+            </TabPanel>
+          </TabPanels>
         </Container>
       </Flex>
     </>
