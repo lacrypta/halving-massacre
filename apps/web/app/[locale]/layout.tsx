d1f4ced6a9837d679fce3c270df8987cb220c252
Jona <llamasjonat@gmail.com>
2024-04-07 19:14:28 -0300

    fix(layout): changed images for metadata

--- apps/web/app/[locale]/layout.tsx
+++ apps/web/app/[locale]/layout.tsx
@@ -36,14 +36,14 @@ export default function RootLayout({ children, params: { locale } }: Readonly<La
         <meta property="og:description" content={t('HEADER_DESC')} />
         <meta property="og:locale" content={`${locale}_${locale.toLocaleUpperCase()}`} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/images/social/facebook-1200x630.jpg" />
+        <meta property="og:image" content="/images/social/facebook.jpg" />
         <meta property="og:url" content="https://lacrypta.ar/" />
 
         {/* Twitter */}
         <meta name="twitter:title" content="Halving Massacre | LaWallet.io" />
         <meta name="twitter:description" content={t('HEADER_DESC')} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/images/social/twitter-600x330.jpg" />
+        <meta name="twitter:image" content="/images/social/twitter.jpg" />
         <meta name="twitter:url" content="https://lacrypta.ar/" />
       </head>
       <body className={inter.className}>
