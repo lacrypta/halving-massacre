7d008477ab55a5fdfa5c2d5eeb4cf282c73f06bc
Jona <llamasjonat@gmail.com>
2024-04-08 16:54:33 -0300

    fix(layout): integrated new metadata

--- apps/web/app/[locale]/layout.tsx
+++ apps/web/app/[locale]/layout.tsx
@@ -48,14 +48,14 @@ export default function RootLayout({ children, params: { locale } }: Readonly<La
         <meta property="og:description" content={t('HEADER_DESC')} />
         <meta property="og:locale" content={`${locale}_${locale.toLocaleUpperCase()}`} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/images/social/facebook-1200x630.jpg" />
+        <meta property="og:image" content="/images/social/facebook.png" />
         <meta property="og:url" content="https://lacrypta.ar/" />
 
         {/* Twitter */}
         <meta name="twitter:title" content="Halving Massacre | LaWallet.io" />
         <meta name="twitter:description" content={t('HEADER_DESC')} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/images/social/twitter-600x330.jpg" />
+        <meta name="twitter:image" content="/images/social/twitter.png" />
         <meta name="twitter:url" content="https://lacrypta.ar/" />
 
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_ID || ''}`} />
