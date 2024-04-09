dd2209c8b083a2ac18dba768ad06ac06645461c6
Jona <llamasjonat@gmail.com>
2024-04-09 16:05:08 -0300

    fix(component): integrated link for Avatar and walias on ItemTxs

--- apps/web/app/[locale]/components/ItemTxs/index.tsx
+++ apps/web/app/[locale]/components/ItemTxs/index.tsx
@@ -1,21 +1,23 @@
+import type { ReactNode } from 'react';
+import Link from 'next/link';
+import { useLocale, useTranslations } from 'next-intl';
+import { formatDistance } from 'date-fns';
+import { enUS } from 'date-fns/locale/en-US';
+import { es } from 'date-fns/locale/es';
 import { Divider, Flex, Text } from '@lawallet/ui';
+import { formatToPreference } from '@lawallet/utils';
+import type { AvailableLanguages } from '@lawallet/utils/types';
 
 import { appTheme } from '../../../../config/exports';
 
 import { Icon } from '../Icon';
+import { AutoAvatar } from '../AutoAvatar';
 
-import { formatToPreference } from '@lawallet/utils';
-import type { AvailableLanguages } from '@lawallet/utils/types';
-import { formatDistance } from 'date-fns';
-import { enUS } from 'date-fns/locale/en-US';
-import { es } from 'date-fns/locale/es';
-import { useLocale, useTranslations } from 'next-intl';
-import type { ReactNode } from 'react';
 import { formatDistanceEN } from '../../../../utils/formatDistance/formatDistanceEN';
 import { formatDistanceES } from '../../../../utils/formatDistance/formatDistanceES';
-import { AutoAvatar } from '../AutoAvatar';
-import { IconStyle, IndicatorStyle, ItemTxsStyle, MessageStyle } from './style';
 import { useRouter } from '../../../../navigation';
+
+import { IconStyle, IndicatorStyle, ItemTxsStyle, MessageStyle } from './style';
 
 interface MessageTxsProps {
   message?: string;
@@ -25,12 +27,13 @@ interface MessageTxsProps {
   text?: string;
   value?: number;
   type?: 'undefined' | 'power' | 'last-zap';
+  href?: string;
 }
 
 const DEFAULT_BUY_TICKET_MESSAGE = 'Your first power!';
 
 export function ItemTxs(props: MessageTxsProps) {
-  const { icon, walias = '', time, text, value = 0, message = '', type = 'undefined' } = props;
+  const { icon, walias = '', time, text, value = 0, message = '', type = 'undefined', href = '' } = props;
   const t = useTranslations();
   const locale = useLocale();
   const router = useRouter();
@@ -40,7 +43,9 @@ export function ItemTxs(props: MessageTxsProps) {
       <Flex align="center" justify="space-between" gap={8}>
         <Flex align="center" gap={8}>
           {walias ? (
-            <AutoAvatar walias={walias} size={7} />
+            <Link href={`/profile/${walias}`} title={walias}>
+              <AutoAvatar walias={walias} size={7} />
+            </Link>
           ) : (
             icon && (
               <IconStyle>
@@ -50,7 +55,9 @@ export function ItemTxs(props: MessageTxsProps) {
           )}
           {type === 'last-zap' ? (
             <Flex direction="column" onClick={() => router.push(`/profile/${walias}`)}>
-              <Text>{walias}</Text>
+              <Link href={`/profile/${walias}`} title={walias}>
+                <Text>{walias}</Text>
+              </Link>
               <Text size="small" color={appTheme.colors.gray50}>
                 {/* {dateFormatter(locale as AvailableLanguages, new Date(time!))} */}
                 {formatDistance(new Date(time!), Date.now(), {
