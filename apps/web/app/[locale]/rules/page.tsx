8c2f53a9fd0e4974a49b5906dd50415a3c0a6906
ferpa <feririarte.1996@gmail.com>
2024-04-09 12:44:15 -0300

    fix rules styles

--- apps/web/app/[locale]/rules/page.tsx
+++ apps/web/app/[locale]/rules/page.tsx
@@ -4,48 +4,6 @@ import { useLocale } from 'next-intl';
 import Rules_ES from './components/ES/Rules_ES';
 import Rules_EN from './components/EN/Rules_EN';
 
-export const tablaStyles: React.CSSProperties = {
-  width: '80%',
-  borderCollapse: 'collapse',
-  margin: 'auto',
-};
-export const thStyles: React.CSSProperties = {
-  border: '1px solid #000000',
-  padding: '8px',
-  backgroundColor: '#f9f9f9',
-  color: '#000000',
-};
-export const tdStyles: React.CSSProperties = {
-  border: '1px solid #000000',
-  padding: '8px',
-  backgroundColor: '#9b9b9b',
-  color: '#000000',
-  whiteSpace: 'nowrap',
-};
-export const tdCenterStyles: React.CSSProperties = {
-  borderLeft: '1px solid #000000',
-  borderRight: '1px solid #000000',
-  borderTop: '2px solid #FF0000',
-  borderBottom: '2px solid #FF0000',
-  padding: '8px',
-  backgroundColor: '#9b9b9b',
-  color: '#000000',
-  whiteSpace: 'nowrap',
-};
-export const listOrderStyles: React.CSSProperties = {
-  width: '100%',
-  listStyleType: 'decimal',
-  textAlign: 'left',
-  margin: '0 auto',
-  paddingLeft: '48px',
-};
-export const listUnorderStyles: React.CSSProperties = {
-  width: '100%',
-  listStyleType: 'disc',
-  margin: '0 auto',
-  paddingLeft: '48px',
-};
-
 export default function Page(): JSX.Element {
   const locale = useLocale();
 
