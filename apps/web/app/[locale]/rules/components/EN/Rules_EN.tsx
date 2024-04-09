c682e9590da93bbcdb7b6da54bcebbbbbcd250fe
ferpa <feririarte.1996@gmail.com>
2024-04-09 12:40:11 -0300

    refactor rules

---
+++ apps/web/app/[locale]/rules/components/EN/Rules_EN.tsx
@@ -0,0 +1,291 @@
+import React from 'react';
+import { Container, Divider, Flex, Heading, Text } from '@lawallet/ui';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { appTheme } from '@/../config/exports';
+import { Navbar } from '@/[locale]/components/Navbar';
+import { listOrderStyles, listUnorderStyles, tablaStyles, tdCenterStyles, tdStyles, thStyles } from '../../page';
+
+const Rules_EN = () => {
+  return (
+    <>
+      <Navbar>
+        <Link href="/">
+          <Image width={23} height={30} alt="Halving Massacre by La Crypta" src={'/images/iso.png'} />
+        </Link>
+      </Navbar>
+      {/* <GameTime round={10} block="820.000" time="20" /> */}
+      <Divider y={16} />
+      <Container>
+        <Heading>Game Rules</Heading>
+        <Divider y={10} />
+        <Text>
+          Survive <b>ALL ROUNDS</b> and get a <b>EPIC treasure</b>.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Summary:</Heading>
+        <Divider y={10} />
+        <Text>
+          Be the last player standing by accumulating sats and increasing your power to take a large percentage of the
+          collected treasure.
+        </Text>
+        <Divider y={10} />
+        <ol style={listOrderStyles}>
+          <li>
+            <Text>Buy your ticket.</Text>
+          </li>
+          <li>
+            <Text>Accumulate sats to gain power.</Text>
+          </li>
+          <li>
+            <Text>Survive the MASSACRE.</Text>
+          </li>
+          <li>
+            <Text>Accumulate MORE sats to gain MORE power.</Text>
+          </li>
+        </ol>
+        <Divider y={20} />
+        <Heading as="h2">How to play?</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>Enter the game with your Lightning Address (aka walias).</Text>
+          </li>
+          <li>
+            <Text>Buy your ticket to participate.</Text>
+          </li>
+          <li>
+            <Text>Accumulate sats to gain power.</Text>
+          </li>
+          <li>
+            <Text>Become the winner of the Halving Massacre thanks to the power of your sats!</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">What is power?</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>Represents the player's strength.</Text>
+          </li>
+          <li>
+            <Text>Increases by zapping sats to the treasure.</Text>
+          </li>
+          <li>
+            <Text>Influences the probability of surviving the MASSACRES.</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Massacres</Heading>
+        <Divider y={10} />
+        <Text>The number of participants is halved every X number of blocks.</Text>
+        <Divider y={10} />
+        {/* Cambiar X por la cantidad de bloques que pactemos */}
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              The hash of the block in which the massacre occurs determines, randomly, the survivors who advance to the
+              next round.
+            </Text>
+          </li>
+          <li>
+            <Text>Half of the participants, rounded down, are the survivors of the massacre.</Text>
+          </li>
+          <li>
+            <Text>The power of the massacred players is distributed among the survivors evenly.</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Players</Heading>
+        <Divider y={20} />
+        <Heading as="h3">Survivors</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>They can continue to accumulate sats to gain more power.</Text>
+          </li>
+          <li>
+            <Text>
+              They receive the power of the massacred divided equally. (total power of the massacred / number of
+              survivors)
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h3">Massacrateds</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>They are eliminated from the game and their power remains in the treasure.</Text>
+          </li>
+          <li>
+            <Text>They may not buy another ticket to re-enter the game.</Text>
+          </li>
+          <li>
+            <Text>Their power is distributed among the survivors.</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Prizes and winners</Heading>
+        <Divider y={10} />
+        <Heading as="h3">Initial treasury</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              La Crypta adds the total of <b>2.100.000 satoshis</b> to the game tresury.
+            </Text>
+          </li>
+          <li>
+            <Text>It is not considered as power, neither in the redistribution nor after the massacres.</Text>
+          </li>
+          <li>
+            <Text>It is distributed to the winners, as appropriate, at the end of the game.</Text>
+          </li>
+        </ul>
+        <Divider y={10} />
+        <Heading as="h3">Game over</Heading>
+        <Divider y={10} />
+        <Text>The game ends when the halving occurs, at block 840,000, when the last MASSACRE occurs.</Text>
+        <Divider y={10} />
+        <Text>The number of winners can be between 4 and 7 depending on the initial number of participants.</Text>
+        <Divider y={20} />
+        <Text>The treasure will be distributed in three levels of winners:</Text>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              <b>50%</b> for the winner. <i>(a single player. See player A in the table below)</i>
+            </Text>
+          </li>
+          <li>
+            <Text>
+              <b>25%</b> for those who are massacred in the final, distributed equally.
+              <i>(between one and two players, not including the winner. See player(s) B in the table below)</i>
+            </Text>
+          </li>
+          <li>
+            <Text>
+              <b>12,5%</b> for those who are massacred in the semifinal, distributed equally.{' '}
+              <i>(between three and four players, not including the finalists. See players C in the table below)</i>
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <blockquote
+          style={{
+            backgroundColor: 'rgba(198, 188, 193, 0.16)',
+            display: 'inline-block',
+            margin: 'auto',
+          }}
+        >
+          The remaining 12.5% is alloted to La Crypta.
+        </blockquote>
+        <Divider y={20} />
+        <Text>The following table shows the progress of the winners once the semifinal round is reached.</Text>
+        <Divider y={10} />
+        <Text>
+          <b>A, B y C:</b> represent players.
+        </Text>
+        <Divider y={10} />
+        <Text>The red lines represent the massacres.</Text>
+        <Divider y={10} />
+        <table style={tablaStyles}>
+          <thead>
+            <tr>
+              <th style={thStyles}>
+                <Text>Treasure percentage</Text>
+              </th>
+              <th style={thStyles}>
+                <Text>Players</Text>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th style={thStyles}>
+                <Text>Winner: 50%</Text>
+              </th>
+              <td style={tdStyles}>
+                <Text>A</Text>
+              </td>
+            </tr>
+            <tr>
+              <th style={thStyles}>
+                <Text>Final: 25%</Text>
+              </th>
+              <td style={tdCenterStyles}>
+                <Text>B B A</Text>
+              </td>
+            </tr>
+            <tr>
+              <th style={thStyles}>
+                <Text>Semifinal: 12,5%</Text>
+              </th>
+              <td style={tdStyles}>
+                <Text>C C C C B B A</Text>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <Divider y={20} />
+        <Text>The satoshis are sent to the Lightning Address (aka walias) of the winners at the end of the game.</Text>
+        <Divider y={20} />
+        <Heading as="h2">Transparency and fair play</Heading>
+        <Divider y={10} />
+        <Text>
+          All transactions are published in NOSTR, achieving transparency and auditability. External manipulations of
+          the game are not allowed.{' '}
+        </Text>
+        {/* Tenemos alguna relay/lugar/forma de que cualquier persona pueda ver de forma facil las transacciones? */}
+        <Divider y={20} />
+        <Heading as="h2">Transaction cost</Heading>
+        <Divider y={10} />
+        <Text>
+          Participants are responsible for the costs of transactions for sending satoshis through the Lightning Network.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Disclaimer</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              There are no refunds of the staked satoshis. The winners will receive the prize in their Lightning Address
+              (aka walias) at the end of the game
+            </Text>
+          </li>
+          <li>
+            <Text>
+              This game involves betting, therefore we are not responsible for how you decide to use your satoshis.
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Subject to changes</Heading>
+        <Divider y={10} />
+        <Text>
+          This rules is subject to change. In any case, sensitive parts such as the distribution of the prize or game
+          algorithms will not be affected, only modifications in terms of clarification of the game.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Open source</Heading>
+        <Divider y={10} />
+        <Text>
+          This is an open source game, so you can check all the details, changes and algorithms used on our GitHub. They
+          can also collaborate.
+        </Text>
+        <Divider y={20} />
+        <Flex justify="center">
+          <Link href="/">
+            <Heading as="h4" color={appTheme.colors.primary}>
+              Let's play!
+            </Heading>
+          </Link>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default Rules_EN;
---
+++ apps/web/app/[locale]/rules/components/ES/Rules_ES.tsx
@@ -0,0 +1,295 @@
+import { Container, Divider, Flex, Heading, Text } from '@lawallet/ui';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { appTheme } from '@/../config/exports';
+import { Navbar } from '@/[locale]/components/Navbar';
+import { listOrderStyles, listUnorderStyles, tablaStyles, tdCenterStyles, tdStyles, thStyles } from '../../page';
+
+const Rules_ES = () => {
+  return (
+    <>
+      <Navbar>
+        <Link href="/">
+          <Image width={23} height={30} alt="Halving Massacre by La Crypta" src={'/images/iso.png'} />
+        </Link>
+        d
+      </Navbar>
+      {/* <GameTime round={10} block="820.000" time="20" /> */}
+      <Divider y={16} />
+      <Container>
+        <Heading>Reglas del juego</Heading>
+        <Divider y={10} />
+        <Text>
+          Sobreviví <b>Todas las rondas</b> y llevate <b>TREMENDO tesoro</b>.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Resumen:</Heading>
+        <Divider y={10} />
+        <Text>
+          Sé el ultimo jugador en pie acumulando sats y aumentando tu poder para llevarte un gran porcentaje del tesoro
+          recaudado.
+        </Text>
+        <Divider y={10} />
+        <ol style={listOrderStyles}>
+          <li>
+            <Text>Comprá tu ticket.</Text>
+          </li>
+          <li>
+            <Text>Acumulá sats para ganar poder.</Text>
+          </li>
+          <li>
+            <Text>Sobreviví a la MASSACRE.</Text>
+          </li>
+          <li>
+            <Text>Acumulá MÁS sats para ganar MÁS poder.</Text>
+          </li>
+        </ol>
+        <Divider y={20} />
+        <Heading as="h2">¿Cómo jugar?</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>Entrá al juego con tu Lightning Address (aka walias).</Text>
+          </li>
+          <li>
+            <Text>Comprá tu ticket para participar.</Text>
+          </li>
+          <li>
+            <Text>Acumulá sats para ganar poder.</Text>
+          </li>
+          <li>
+            <Text>Convertite en el ganador del Halving Massacre gracias al poder de tus sats!</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">¿Qué es el poder?</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>Representa la fuerza del jugador.</Text>
+          </li>
+          <li>
+            <Text>Incrementa zappeando sats al tesoro.</Text>
+          </li>
+          <li>
+            <Text>Influye en la probabilidad de sobrevivir a las MASSACRES.</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Massacres</Heading>
+        <Divider y={10} />
+        <Text>La cantidad de participantes se reduce a la mitad cada X cantidad de bloques.</Text>
+        <Divider y={10} />
+        {/* Cambiar X por la cantidad de bloques que pactemos */}
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              El hash del bloque en el cual se hace la massacre determina, de manera aleatoria, los sobrevivientes que
+              avanzan a la siguiente ronda.
+            </Text>
+          </li>
+          <li>
+            <Text>La mitad de los participantes, redondeando para abajo, son los sobrevivientes de la massacre.</Text>
+          </li>
+          <li>
+            <Text>
+              El poder de los jugadores massacrados es distribuido entre los sobrevivientes de manera igualitaria.
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Jugadores</Heading>
+        <Divider y={20} />
+        <Heading as="h3">Sobrevivientes</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>Pueden seguir acumulando sats para ganar más poder.</Text>
+          </li>
+          <li>
+            <Text>
+              Reciben el poder de los massacrados dividido en partes iguales. (poder total de los massacrados / cantidad
+              de sobrevivientes)
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h3">Massacrados</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>Quedan eliminados del juego y su poder permanece en el tesoro.</Text>
+          </li>
+          <li>
+            <Text>No pueden volver a comprar un ticket para re-ingresar al juego</Text>
+          </li>
+          <li>
+            <Text>Su poder es distribuido entre los sobrevivientes.</Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Premios y ganadores</Heading>
+        <Divider y={10} />
+        <Heading as="h3">Tesoro inicial</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              La Crypta suma el total de <b>2.100.000 satoshis</b> al tesoro del juego.
+            </Text>
+          </li>
+          <li>
+            <Text>No es tomado en cuenta como poder ni en la redistribución luego de las massacres.</Text>
+          </li>
+          <li>
+            <Text>Se distribuye a los ganadores, segun corresponda, al finalizar el juego.</Text>
+          </li>
+        </ul>
+        <Divider y={10} />
+        <Heading as="h3">Fin del juego</Heading>
+        <Text>El juego termina cuando ocurre el halving, en el bloque 840.000, cuando sucede la ultima MASSACRE.</Text>
+        <Divider y={10} />
+        <Text>La cantidad de ganadores puede ser entre 4 y 7 dependiendo la cantidad de participantes iniciales.</Text>
+        <Divider y={20} />
+        <Text>El tesoro se distribuirá de la siguiente manera:</Text>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              <b>50%</b> para el ganador. <i>(un solo jugador. Véase el jugador A en la tabla de abajo)</i>
+            </Text>
+          </li>
+          <li>
+            <Text>
+              <b>25%</b> para los que hayan sido massacrados en la final, distribuido igualitariamente.
+              <i>(entre uno y dos jugadores. Véase el/los jugadores B en la tabla de abajo)</i>
+            </Text>
+          </li>
+          <li>
+            <Text>
+              <b>12,5%</b> para los que hayan sido massacrados en la semifinal, distribuido igualitariamente.{' '}
+              <i>(entre tres y cuatro jugadores. Véase los jugador C en la tabla de abajo)</i>
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <blockquote
+          style={{
+            backgroundColor: 'rgba(198, 188, 193, 0.16)',
+            display: 'inline-block',
+            margin: 'auto',
+          }}
+        >
+          El restante 12,5% va destinado para La Crypta.
+        </blockquote>
+        <Divider y={20} />
+        <Text>La siguiente tabla muestra el avance de los ganadores una vez alcanzada la ronda de semifinal.</Text>
+        <Divider y={10} />
+        <Text>
+          <b>A, B y C:</b> representan jugadores.
+        </Text>
+        <Divider y={10} />
+        <Text>Las lineas rojas representan las massacres.</Text>
+        <Divider y={10} />
+        <table style={tablaStyles}>
+          <thead>
+            <tr>
+              <th style={thStyles}>
+                <Text>Porcentaje del tesoro</Text>
+              </th>
+              <th style={thStyles}>
+                <Text>Jugadores</Text>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th style={thStyles}>
+                <Text>Ganador: 50%</Text>
+              </th>
+              <td style={tdStyles}>
+                <Text>A</Text>
+              </td>
+            </tr>
+            <tr>
+              <th style={thStyles}>
+                <Text>Final: 25%</Text>
+              </th>
+              <td style={tdCenterStyles}>
+                <Text>B B A</Text>
+              </td>
+            </tr>
+            <tr>
+              <th style={thStyles}>
+                <Text>Semifinal: 12,5%</Text>
+              </th>
+              <td style={tdStyles}>
+                <Text>C C C C B B A</Text>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <Divider y={20} />
+        <Text>
+          Los satoshis son enviados a la Lightning Address (aka walias) de los ganadores al finalizar el juego.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Transparencia y juego limpio</Heading>
+        <Divider y={10} />
+        <Text>
+          Todas las transacciones son publicadas en NOSTR. Logrando así transparencia y auditabilidad. Las
+          manipulaciones externas del juego no están permitidas.{' '}
+        </Text>
+        {/* Tenemos alguna relay/lugar/forma de que cualquier persona pueda ver de forma facil las transacciones? */}
+        <Divider y={20} />
+        <Heading as="h2">Coste de transacciones</Heading>
+        <Divider y={10} />
+        <Text>
+          Los participantes son los responsables de los costos de las transacciones por enviar satoshis a través de
+          Lightning Network.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Descargo de responsabilidad</Heading>
+        <Divider y={10} />
+        <ul style={listUnorderStyles}>
+          <li>
+            <Text>
+              No se realizan reembolsos de los satoshis apostados. Los ganadores recibiran el premio en su Lightning
+              Address (aka walias) al finalizar el juego
+            </Text>
+          </li>
+          <li>
+            <Text>
+              Este juego implica apuestas, por lo tanto no nos responsabilizamos como decidas utilizar tus satoshis.
+            </Text>
+          </li>
+        </ul>
+        <Divider y={20} />
+        <Heading as="h2">Sujeto a cambios</Heading>
+        <Divider y={10} />
+        <Text>
+          Este reglamento esta sujeto a cambios. De todas maneras, no afectarán partes sensibles como la distribución
+          del premio ni algoritmos del juego, solo modificaciones en cuanto a clarificación del juego.
+        </Text>
+        <Divider y={20} />
+        <Heading as="h2">Código abierto</Heading>
+        <Divider y={10} />
+        <Text>
+          Este es un juego de código abierto, por lo tanto pueden verificar todos los detalles, cambios y algoritmos
+          utilizados en nuestro GitHub. También pueden colaborar.
+        </Text>
+        <Divider y={20} />
+        <Flex justify="center">
+          <Link href="/">
+            <Heading as="h4" color={appTheme.colors.primary}>
+              ¡A jugar!
+            </Heading>
+          </Link>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default Rules_ES;
