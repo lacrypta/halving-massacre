28684dae55319c432dc46aab0c5135057e4457a0
Rapax <gasparpelayo44@gmail.com>
2024-04-10 23:26:20 -0300

    feat(component): Added animation to logo

--- apps/web/app/[locale]/components/Header/index.tsx
+++ apps/web/app/[locale]/components/Header/index.tsx
@@ -5,14 +5,58 @@ import { useTranslations } from 'next-intl';
 
 import pngLogo from '../../../../public/images/logo.png';
 import Image from 'next/image';
+import { useState } from 'react';
+import { ImagesContainer, ImageSubContainer, ImageM1, ImageSword, ImageM2 } from './style';
 
 export default function Header() {
   const t = useTranslations();
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <HeaderPrimitive>
       <Flex align="center" justify="center" direction="column">
-        <Image width={240} height={77} alt="Halving Massacre by La Crypta" src={pngLogo.src} />
+        <ImagesContainer onMouseEnter={() => setIsHovered(true)} onAnimationEnd={() => setIsHovered(false)}>
+          <ImageSubContainer>
+            <ImageSword
+              width={35}
+              height={93}
+              isAnimating={isHovered}
+              alt="Sword - Halving Massacre by La Crypta"
+              src={'/images/logoAnimated/sword.png'}
+            />
+            <ImageM1
+              width={51}
+              height={34}
+              alt="M1 - Halving Massacre by La Crypta"
+              src={'/images/logoAnimated/m1.png'}
+            />
+            <ImageM2
+              width={51}
+              height={34}
+              alt="M2 - Halving Massacre by La Crypta"
+              src={'/images/logoAnimated/m2.png'}
+            />
+          </ImageSubContainer>
+          <ImageSubContainer>
+            <Image
+              width={190}
+              height={39}
+              style={{ marginLeft: '-12px' }}
+              alt="Halving - Halving Massacre by La Crypta"
+              src={'/images/logoAnimated/halving.png'}
+            />
+            <Image
+              width={203}
+              height={34}
+              style={{ marginTop: '5px' }}
+              alt="Massacre - Halving Massacre by La Crypta"
+              src={'/images/logoAnimated/assacre.png'}
+            />
+          </ImageSubContainer>
+        </ImagesContainer>
+        {/*Static image
+           <Image width={240} height={77} alt="Halving Massacre by La Crypta" src={pngLogo.src} />
+         */}
         <Divider y={12} />
         <Text align="center">{t('HEADER_DESC')}</Text>
         <Text align="center">{t('HEADER_DESC2')}</Text>
--- apps/web/app/[locale]/components/Header/style.tsx
+++ apps/web/app/[locale]/components/Header/style.tsx
@@ -1,5 +1,46 @@
-import { styled } from 'styled-components';
+import { styled, keyframes, css } from 'styled-components';
 
 export const HeaderPrimitive = styled.div`
   max-width: 400px;
+`;
+
+const upAndDown = keyframes`
+  0% { transform: translateY(0) translateX(0)}
+  50% { transform: translateY(-40px) translateX(-10px); }
+  100% { transform: translateY(0) translateX(0); }
+`;
+
+export const ImagesContainer = styled.div`
+  display: flex;
+`;
+
+export const ImageSubContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ImageM1 = styled.img`
+  position: relative;
+  margin-top: -40px;
+  margin-left: 0;
+  z-index: 2;
+`;
+
+export const ImageSword = styled.img<{ isAnimating: boolean }>`
+  position: relative;
+  margin-top: -10px;
+  margin-left: 13px;
+  z-index: 1;
+  ${({ isAnimating }) =>
+    isAnimating &&
+    css`
+      animation: ${upAndDown} 1s forwards;
+    `}
+`;
+
+export const ImageM2 = styled.img`
+  position: relative;
+  margin-top: -34px;
+  margin-left: 0px;
+  z-index: 0;
 `;
