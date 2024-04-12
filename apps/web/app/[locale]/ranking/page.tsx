8009ff926a0010866312678a60d76a49582b2e0b
Jona <llamasjonat@gmail.com>
2024-04-12 14:19:08 -0300

    fix(page): integrated total sats on Ranking

--- apps/web/app/[locale]/ranking/page.tsx
+++ apps/web/app/[locale]/ranking/page.tsx
@@ -2,16 +2,22 @@
 
 // Libraries
 import React, { useState } from 'react';
-import Image from 'next/image';
-import { Container, Divider, Flex, Heading } from '@lawallet/ui';
+import { useLocale, useTranslations } from 'next-intl';
+import { Container, Divider, Flex, Heading, Text } from '@lawallet/ui';
+import { useFormatter } from '@lawallet/react';
+import type { AvailableLanguages } from '@lawallet/utils/types';
 
+// Theme
+import { appTheme } from '../../../config/exports';
+
+// Hooks and utils
 import { useMassacre } from '../../../hooks/useMassacre';
 
 // Generic components
 import { Navbar } from '../components/Navbar';
+import Card from '../components/Card';
+import Treasury from '../components/TreasuryAnimation';
 
-import pngIso from '../../../public/images/iso.png';
-import { Link } from '../../../navigation';
 import { RankingList } from '../components/RankingList';
 import { Tab, TabList, Tabs, TabPanel, TabPanels } from '../components/Tabs';
 import { GameTime } from '../components/GameTime';
@@ -23,8 +29,16 @@ interface PageProps {
 }
 
 export default function Page({ params }: PageProps): JSX.Element {
-  const { top100Players: players } = useMassacre();
+  // Generics
+  const t = useTranslations();
+  const locale = useLocale() as AvailableLanguages;
 
+  // Context
+  const { top100Players: players, currentPool } = useMassacre();
+  const { formatAmount } = useFormatter({ currency: 'SAT', locale });
+  const totalPrice = formatAmount(currentPool / 1000);
+
+  // Mocks
   const [nameTab, setNameTab] = useState('global');
 
   return (
@@ -34,6 +48,13 @@ export default function Page({ params }: PageProps): JSX.Element {
       <Divider y={16} />
       <Flex direction="column" align="center">
         <Heading as="h1">Ranking</Heading>
+        <Divider y={16} />
+        <Container>
+          <Card animation={<Treasury />} size="small">
+            <Heading as="h2">{totalPrice}</Heading>
+            <Text color={appTheme.colors.gray50}>{t('TOTAL_PRIZE')}.</Text>
+          </Card>
+        </Container>
         <Divider y={16} />
         <Heading align="center" as="h3">
           Top 100
